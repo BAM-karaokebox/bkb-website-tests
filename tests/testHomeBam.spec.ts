dbca9282d0804498ed9c0cca70394b3776ce1996
Laurent Malvert <laurent.malvert.freelance@gmail.com>
2022-03-14 19:59:51 +0100

    BAM-900 create homepage test suite with reusable before hook + fix encoding
     * change file to use UNIX encoding system instead of DOS
    
     * create a test suite
    
     * leverage test hook to reuse first pageload instruction
    
     * externalize BASE_URL to variable

--- tests/testHomeBam.spec.ts
+++ tests/testHomeBam.spec.ts
@@ -1,51 +1,56 @@
 import { test, expect } from '@playwright/test';
 
-test('Visibilité du logo', async ({ page }) => {
-  await page.goto('https://fr.bam-karaokebox.com/');
-  const imagelocator = page.locator(".header__brand");
-  await expect(imagelocator).toBeVisible('http://www.w3.org/2000/svg');
-});
 
-test('Boutton Réserver en bordure ', async ({ page }) => {
-  await page.goto('https://fr.bam-karaokebox.com/');
-  const ReserverBoutton = page.locator(".header__right");
-  await expect(ReserverBoutton).toContainText('Réserver');
-});
+const BASE_URL = 'https://fr.bam-karaokebox.com/';
 
-test('Click burger', async ({ page }) => {
-    await page.goto('https://fr.bam-karaokebox.com/');
+
+test.describe('Homepage', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
+  });
+
+
+  test('Visibilité du logo', async ({ page }) => {
+    const imagelocator = page.locator(".header__brand");
+    await expect(imagelocator).toBeVisible('http://www.w3.org/2000/svg');
+  });
+
+  test('Boutton Réserver en bordure ', async ({ page }) => {
+    const ReserverBoutton = page.locator(".header__right");
+    await expect(ReserverBoutton).toContainText('Réserver');
+  });
+
+  test('Click burger', async ({ page }) => {
     await page.click(".burger__wrap");
-});
+  });
 
-test('Check listed menu', async ({ page }) => {
-    await page.goto('https://fr.bam-karaokebox.com/');
+  test('Check listed menu', async ({ page }) => {
     const Listlocator = page.locator('.header__content--wrap');
     await expect(Listlocator).toContainText(['Paris', 'Bordeaux', 'Madrid']);
     await expect(Listlocator).toContainText(['fr','en','es']);
-});
+  });
 
-test('About Bam KaraokeBox', async ({ page }) => {
-  await page.goto('https://fr.bam-karaokebox.com/');
-  const AboutUsText = page.locator('.push__wrap');
-  await expect(AboutUsText).toBeVisible();
-  const AboutUsImage = page.locator('.push__image');
-  await expect(AboutUsImage).toBeVisible();
-});
+  test('About Bam KaraokeBox', async ({ page }) => {
+    const AboutUsText = page.locator('.push__wrap');
+    await expect(AboutUsText).toBeVisible();
+    const AboutUsImage = page.locator('.push__image');
+    await expect(AboutUsImage).toBeVisible();
+  });
 
-test('Les différents lieux', async ({ page }) => {
-  await page.goto('https://fr.bam-karaokebox.com/');
-  const AboutUsText = page.locator('id=places');
-  await expect(AboutUsText).toBeVisible();
-});
+  test('Les différents lieux', async ({ page }) => {
+    const AboutUsText = page.locator('id=places');
+    await expect(AboutUsText).toBeVisible();
+  });
 
-test('Social Container', async ({ page }) => {
-    await page.goto('https://fr.bam-karaokebox.com/');
+  test('Social Container', async ({ page }) => {
     const sociallocator = page.locator('id=press');
     await expect(sociallocator).toBeVisible();
   });
 
-test('Instagram Container', async ({ page }) => {
-    await page.goto('https://fr.bam-karaokebox.com/');
+  test('Instagram Container', async ({ page }) => {
     const instagramlocator = page.locator('id=instagram');
     await expect(instagramlocator).toBeVisible();
+  });
+
 });
