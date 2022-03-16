5e63396feb4aad33e69d41427a6d8e4a7bcf8136
Thomas <Thomas@mbp-de-thomas.home>
2022-03-16 13:34:37 +0100

    BAM-901 Update testPlaylist.spec.ts

--- website-test/tests/testPlaylist.spec.ts
+++ website-test/tests/testPlaylist.spec.ts
@@ -1,25 +1,34 @@
 import { test, expect } from '@playwright/test';
 
 
-test('Visibilité du container de la Playlist', async ({ page }) => {
+test('Visibilité/Fonctionnement du container Playlist', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com')
     const modalCloseButton = await page.$$('.modal-close');
     if (modalCloseButton) {
       await page.click('.modal-close');
     }
-    const PlaylistContainer = page.locator(".catalog__thumbs");
+
+    //Visibilité des differents playlist
+    const PlaylistContainer = page.locator('.catalog__thumbs');
     await expect(PlaylistContainer).toBeVisible();
 
-    const SearchContainer = page.locator(".catalog__search");
+    //Visibilité/Fonctionnement de la barre de recherche
+    const SearchContainer = page.locator('.catalog__search');
     await expect(SearchContainer).toBeVisible();
+    await page.fill('[type="search"]', 'Au DD')
+    await page.keyboard.press('Enter');
 
-    await page.fill('[type="search"]', "Au DD")
-    await page.keyboard.press("Enter");
-    const SongContainer = page.locator(".catalog__songs--home");
+    //Visibilité du contenu des playlists ou de la recherche
+    const SongContainer = page.locator('.catalog__songs--home');
     await expect(SongContainer).toBeVisible();
 
+    //Visibilité du contenu de la playlist personnalisé
+    const SongClient = page.locator('.catalog__songs--client');
+    await expect(SongClient).toBeVisible();    
     await page.click('.song__controls')
-    const ButtonReset = page.locator(".button-reset");
+
+    //Visibilité/Fonctionnement du boutton reset
+    const ButtonReset = page.locator('.u-txt-right .button-reset');
     await expect(ButtonReset).toBeVisible();
-    await page.click('.button-reset')
+    await page.click('.u-txt-right .button-reset')
 });
