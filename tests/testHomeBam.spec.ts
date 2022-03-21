88adebce84b5f8ca7d5c1ae0409d9af0c37b5baa
Tomas Veschambre <tomas.vesch@gmail.com>
2022-03-21 13:34:38 +0100

    BAM-901 Fix lint errors

--- tests/testHomeBam.spec.ts
+++ tests/testHomeBam.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-
 const BASE_URL = 'https://fr.bam-karaokebox.com/';
-
 
 test.describe('Homepage', () => {
 
@@ -12,28 +10,27 @@ test.describe('Homepage', () => {
     // trigger a "useless" click to close temporary modals we may have
     // at the moment on our website
     // (e.g. special events like K-Pop nights)
-    await page.click("body");
+    await page.click('body');
   });
 
-
   test('Visibilité du logo', async ({ page }) => {
-    const imagelocator = page.locator(".header__brand");
+    const imagelocator = page.locator('.header__brand');
     await expect(imagelocator).toBeVisible('http://www.w3.org/2000/svg');
   });
 
   test('Boutton Réserver en bordure ', async ({ page }) => {
-    const ReserverBoutton = page.locator(".header__right");
+    const ReserverBoutton = page.locator('.header__right');
     await expect(ReserverBoutton).toContainText('Réserver');
   });
 
   test('Click burger', async ({ page }) => {
-    await page.click(".burger__wrap");
+    await page.click('.burger__wrap');
   });
 
   test('Check listed menu', async ({ page }) => {
     const Listlocator = page.locator('.header__content--wrap');
     await expect(Listlocator).toContainText(['Paris', 'Bordeaux', 'Madrid']);
-    await expect(Listlocator).toContainText(['fr','en','es']);
+    await expect(Listlocator).toContainText(['fr', 'en', 'es']);
   });
 
   test('About Bam KaraokeBox', async ({ page }) => {
--- tests/testVenues.spec.ts
+++ tests/testVenues.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-
-
 test('Paris Richer', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/richer');
     const Presentationlocator = page.locator('.hero');
@@ -13,12 +11,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Sentier', async ({ page }) => {
+
+test('Paris Sentier', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/sentier');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -29,12 +27,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Parmentier', async ({ page }) => {
+
+test('Paris Parmentier', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/parmentier');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -45,12 +43,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Madeleine', async ({ page }) => {
+
+test('Paris Madeleine', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/madeleine');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -61,12 +59,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Etoile', async ({ page }) => {
+
+test('Paris Etoile', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/etoile');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -77,12 +75,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Chartrons', async ({ page }) => {
+
+test('Paris Chartrons', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/chartrons');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -93,12 +91,12 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
-  test('Paris Recoletos', async ({ page }) => {
+
+test('Paris Recoletos', async ({ page }) => {
     await page.goto('https://fr.bam-karaokebox.com/etablissement/recoletos');
     const Presentationlocator = page.locator('.hero');
     await expect(Presentationlocator).toBeVisible();
@@ -109,8 +107,7 @@ test('Paris Richer', async ({ page }) => {
     const Infoslocator = page.locator('id=practical-infos');
     await expect(Infoslocator).toBeVisible();
     const Presslocator = page.locator('id=press');
-    await expect(Presslocator).toBeVisible(); 
+    await expect(Presslocator).toBeVisible();
     const Faqlocator = page.locator('id=faq');
     await expect(Faqlocator).toBeVisible();
   });
-  
