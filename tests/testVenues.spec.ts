848c59ee5020ca608a4bdb7995b53f9ecdcefab6
Laurent Malvert <laurent.malvert.freelance@gmail.com>
2022-12-30 14:39:16 +0100

    BAM-1443 fix: add Luchana to list of checked veenues

--- tests/testVenues.spec.ts
+++ tests/testVenues.spec.ts
@@ -3,7 +3,7 @@ import { test, expect, Page } from "@playwright/test";
 const BASE_URL = "https://fr.bam-karaokebox.com";
 const TEST_MARKER_SUFFIX = "?utm_source=bkb-website-tests&utm_medium=qa-bot&utm_campaign=monitoring";
 
-const VENUES = ["richer", "sentier", "parmentier", "madeleine", "etoile", "chartrons", "recoletos"];
+const VENUES = ["richer", "sentier", "parmentier", "madeleine", "etoile", "chartrons", "recoletos", "luchana"];
 
 const checkVenuePageLayout = async (page: Page, venuePath: string) => {
   await page.goto(BASE_URL + venuePath + TEST_MARKER_SUFFIX);
